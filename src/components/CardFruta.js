a61fbf2243e6682aa436aed30e1973c04fefe1c7
Euagle <gleiscylima@gmail.com>
2022-11-30 19:12:12 -0300

    concluído

--- src/components/CardFruta.js
+++ src/components/CardFruta.js
@@ -1,14 +1,15 @@
 import React from "react";
 import styled from "styled-components";
 
-export default function CardFruta({ props }) {
-  const {fruta, comprar} = props;
+export default function CardFruta(props) {
+  const { fruta, comprar } = props;
+
   return (
     <CardContainer>
       <Image src={fruta.url} alt={fruta.name} />
-      <p>{fruta.name}</p>s
+      <p>{fruta.name}</p>
       <p>R${fruta.price}</p>
-      <button onClick={()=> comprar(fruta.id)}>Comprar</button>
+      <button onClick={() => comprar(fruta.id)}>Comprar</button>
     </CardContainer>
   );
 }
